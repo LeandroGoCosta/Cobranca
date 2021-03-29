f7d5e7b9be6c08e064d1b8186a361749794add21
Leandro <leandro.gocosta@gmail.com>
2021-03-29 17:39:55 -0300

    Primeiro commit projeto

---
+++ src/main/java/com/inuvens/cobranca/repository/Titulos.java
@@ -0,0 +1,11 @@
+package com.inuvens.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.inuvens.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+	
+	
+}
